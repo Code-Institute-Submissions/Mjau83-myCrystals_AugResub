c2edee26da3dc07cd2bdaaf016d43b6207402194
Mjau83 <mamoni.hemlin@gmail.com>
2021-05-21 13:03:05 +0000

    Add user profile

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ def register():
 
         session["user"] = request.form.get("username").lower()
         flash("You are registered and can start to add your crystals!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -59,7 +60,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome {}".format(request.form.get("username")))
+                    flash("Welcome {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 # invalid passwod
                 flash("The Username and/or Password is incorrect. Please try again")
@@ -71,6 +75,14 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab only the user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
