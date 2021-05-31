34000e60bc1e258e885627a0c5af58beda0199d0
Mjau83 <mamoni.hemlin@gmail.com>
2021-05-31 11:01:43 +0000

    Add materialize datepicker

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.tooltipped').tooltip();
     $('select').formSelect();
+    $('.datepicker').datepicker();
   });
 
 var elem = document.querySelector('.collapsible.expandable');
